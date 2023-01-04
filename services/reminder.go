9c0c307522e2efbace5a43c7d1b3fd3832978657
c1337 <cyril199897@gmail.com>
2023-01-04 22:41:04 +0530

    Reminder logic

---
+++ services/reminder.go
@@ -0,0 +1,29 @@
+package services
+
+import (
+	"time"
+
+	"go.mau.fi/whatsmeow/types"
+)
+
+type Reminder struct {
+	Message string
+	To      types.JID
+	Time 	time.Time
+
+}
+
+var reminderList = make(map[time.Time][]Reminder)
+
+
+func addReminder(reminder Reminder){
+	
+	reminderList[reminder.Time] = append(reminderList[reminder.Time], reminder)
+}
+
+func getReminders(time time.Time)( []Reminder){
+	remindersAtTime:= reminderList[time]
+	return remindersAtTime
+}
+
+
